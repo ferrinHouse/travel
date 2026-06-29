ab8a0d7a56571a9ee8724879928ba22c008476c1
whwar9739 <ferrin.michael.84@gmail.com>
2026-06-28 20:35:23 -0500

    fix: update seed.js to target DATABASE_URL environment variable

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const { PrismaBetterSqlite3 } = require('@prisma/adapter-better-sqlite3');
 
-const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' });
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL || 'file:./dev.db' });
 const prisma = new PrismaClient({ adapter });
 
 
